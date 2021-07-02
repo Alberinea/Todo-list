f90b31389aad0767ea037ea1382f93677290f5ab
Alberinea <kan9870@gmail.com>
2021-07-02 18:12:18 +0900

    Add datepicker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const todo = (description, dueDate, priority, project) => {
     return { description, dueDate, priority, project };
 };
@@ -11,14 +10,28 @@ const addTask = (() => {
         document.getElementById('formSubmit').style.display = 'flex';
         document.getElementById('add').style.display = 'none';
     };
+    const reset = () => {
+        document.getElementById('taskText').value = '';
+        document.getElementById('datepicker').value = '';
+    };
     const hideInterface = () => {
         document.getElementById('form').style.display = 'none';
         document.getElementById('formSubmit').style.display = 'none';
         document.getElementById('add').style.display = 'flex';
+        reset()
     };
     const getTask = () => {
         const description = document.getElementById('taskText').value;
-        const dueDate = 'Today';
+        const dueDate =
+            new Date()
+                .toLocaleDateString(undefined, {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                })
+                .replace(/\//g, '-') === document.getElementById('datepicker').value
+                ? 'Today'
+                : document.getElementById('datepicker').value;
         const priority = '!';
         const project = 'default';
         return todo(description, dueDate, priority, project);
@@ -34,7 +47,7 @@ const addTask = (() => {
     const pushArray = () => {
         taskArray.push(getTask());
     };
-    const addTaskToDoc = () => {
+    const DOMTask = () => {
         if (getTask().description === '') return;
         document.getElementById('form').before(document.createElement('li'));
         const lastList = document.querySelectorAll('li')[document.querySelectorAll('li').length - 1];
@@ -42,8 +55,8 @@ const addTask = (() => {
         const div2 = lastList.appendChild(document.createElement('div'));
         const bullet = div1.appendChild(document.createElement('div'));
         const text = div1.appendChild(document.createElement('p'));
-        const project = div2.appendChild(document.createElement('div'));
         const date = div2.appendChild(document.createElement('p'));
+        const project = div2.appendChild(document.createElement('div'));
         const priority = div2.appendChild(document.createElement('p'));
         div1.className = 'listInside';
         div2.className = 'listInside';
@@ -52,20 +65,20 @@ const addTask = (() => {
         date.className = 'date';
         priority.className = 'priority';
         text.textContent = getTask().description;
-        project.textContent = getTask().project;
         date.textContent = getTask().dueDate;
+        project.textContent = getTask().project;
         priority.textContent = getTask().priority;
         priority.style.color =
             getTask().priority.length === 1 ? 'green' : getTask().priority.length === 2 ? 'yellow' : 'red';
         pushArray();
         changeOpacity();
-        document.getElementById('taskText').value = '';
+        reset();
         bullet.addEventListener('click', taskDone);
     };
     const addListener = () => {
         document.getElementById('add').addEventListener('click', showInterface);
         document.getElementById('cancel').addEventListener('click', hideInterface);
-        document.getElementById('submit').addEventListener('click', addTaskToDoc);
+        document.getElementById('submit').addEventListener('click', DOMTask);
         document.getElementById('taskText').addEventListener('keyup', changeOpacity);
     };
     return { addListener };
